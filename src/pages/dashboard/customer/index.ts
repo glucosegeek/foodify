cc4a2e627c53ddd9ea757bedeba3b814bbb38b13
glucosegeek <glucosegeek@proton.me>
2025-09-29 20:19:31 +0200

    Updated index.ts

---
+++ src/pages/dashboard/customer/index.ts
@@ -0,0 +1,4 @@
+// Customer pages exports
+export { CustomerProfile } from './CustomerProfile';
+export { CustomerActivities } from './CustomerActivities';
+export { CustomerFollowing, CustomerFavorites, CustomerNotifications, CustomerSettings } from './CustomerPages';
