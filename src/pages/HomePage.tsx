0ea642aac9784aa4590d3ce5ab1546b214daa627
glucosegeek <glucosegeek@proton.me>
2025-10-01 19:43:52 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { HeroSection } from '../components/home/HeroSection';
 import { FeaturedRestaurants } from '../components/home/FeaturedRestaurants';
 
 export function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Handle scroll to hash on page load
+    if (location.hash === '#top-restaurants') {
+      setTimeout(() => {
+        document.getElementById('top-restaurants')?.scrollIntoView({ 
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }, 100);
+    }
+  }, [location]);
+
   return (
     <div>
       <HeroSection />
