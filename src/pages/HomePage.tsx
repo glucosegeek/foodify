61b1ba8e68e4bfecbbf0f76a8719b66290e2d12c
glucosegeek <glucosegeek@proton.me>
2025-10-05 18:02:24 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,39 +1,10 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { HeroSection } from '../components/home/HeroSection';
-import { FeaturedRestaurants } from '../components/home/FeaturedRestaurants';
-
+// src/pages/HomePage.tsx - TEST
 export function HomePage() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.hash) {
-      const targetId = location.hash.replace('#', '');
-      requestAnimationFrame(() => {
-        setTimeout(() => {
-          const element = document.getElementById(targetId);
-          if (element) {
-            const header = document.querySelector('header');
-            const headerHeight = header?.offsetHeight || 64;
-            const elementPosition = element.getBoundingClientRect().top;
-            const offsetPosition = elementPosition + window.pageYOffset - headerHeight;
-            
-            window.scrollTo({
-              top: offsetPosition,
-              behavior: 'smooth'
-            });
-          }
-        }, 150);
-      });
-    } else {
-      window.scrollTo({ top: 0, behavior: 'instant' });
-    }
-  }, [location.hash, location.pathname]);
-
   return (
     <div>
       <HeroSection />
-      <FeaturedRestaurants />
+      {/* <FeaturedRestaurants /> */}
+      <p>TEST - HomePage works!</p>
     </div>
   );
 }
